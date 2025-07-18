996e9d91a6bdbd1a8ce0856542779ae8e359d2b0
mateusscm <mateus.miranda@hotmail.com.br>
2025-07-18 15:49:02 -0300

    feat: update city and state validation to require a minimum of 3 characters and add filter schema

--- src/server/schemas/cities/createCity.schema.ts
+++ src/server/schemas/cities/createCity.schema.ts
@@ -6,20 +6,28 @@ export const createCitySchema = z.object({
       required_error: "Name is required",
       invalid_type_error: "Name must be a string",
     })
-    .min(1, "Name is required")
-    .min(2, "City name must be at least 2 characters long")
+    .min(3, "City name must be at least 3 characters long")
     .max(50, {
-      message: "City name must be between 2 and 50 characters long",
+      message: "City name must be between 3 and 50 characters long",
     }),
   state: z
     .string({
       required_error: "State is required",
       invalid_type_error: "State must be a string",
     })
-    .min(2, "State must be at least 2 characters long")
+    .min(3, "State must be at least 3 characters long")
     .max(50, {
-      message: "State must be between 2 and 50 characters long",
+      message: "State must be between 3 and 50 characters long",
     }),
 });
 
+export const filterSchema = z.object({
+  filter: z
+    .string()
+    .min(3, "Filter must be at least 3 characters long")
+    .nonempty(),
+  limit: z.number().int().min(1, "Limit must be at least 1").nonnegative(),
+});
+
 export type CreateCityInput = z.infer<typeof createCitySchema>;
+export type FilterInput = z.infer<typeof filterSchema>;
