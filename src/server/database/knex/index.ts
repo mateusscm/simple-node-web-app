7ee776d682c606e2f8d1f9c2efb07eeca8668d6f
mateusscm <mateus.miranda@hotmail.com.br>
2025-07-24 20:58:59 -0300

    feat: add database configuration and migration for city creation

---
+++ src/server/database/knex/index.ts
@@ -0,0 +1,10 @@
+import { knex } from "knex";
+import { development, production, test } from "./Environment";
+
+const getEnv = () => {
+  if (process.env.NODE_ENV === "production") return production;
+  if (process.env.NODE_ENV === "test") return test;
+  return development;
+};
+
+export const Knex = knex(getEnv());
