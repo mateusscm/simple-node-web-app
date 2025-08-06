273a5e4b6824b16eae356c5d4427d42509135d3f
mateusscm <mateus.miranda@hotmail.com.br>
2025-08-06 18:45:10 -0300

    style: format connection object in development configuration for better readability

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -1,19 +1,22 @@
+/* eslint-disable indent */
 import { Knex } from "knex";
 import "ts-node/register";
 import path from "path";
 
 export const development: Knex.Config = {
   client: "pg",
-  connection: process.env.DATABASE_URL ? {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  } : {
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "postgres",
-    database: process.env.DB_NAME || "node_api_dev",
-  },
+  connection: process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST || "localhost",
+        port: Number(process.env.DB_PORT) || 5432,
+        user: process.env.DB_USER || "postgres",
+        password: process.env.DB_PASSWORD || "postgres",
+        database: process.env.DB_NAME || "node_api_dev",
+      },
   migrations: {
     directory: path.resolve(__dirname, "..", "migrations"),
   },
