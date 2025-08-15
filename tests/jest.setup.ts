ce13607048a8194d5898672151e65f0e7a567b1d
mateusscm <mateus.miranda@hotmail.com.br>
2025-08-15 17:28:16 -0300

    feat: add helper function to create a test city in jest setup

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,4 +1,5 @@
 import supertest from "supertest";
+import { StatusCodes } from "http-status-codes";
 import { server } from "../src/server/Server";
 import { Knex } from "../src/server/database/knex";
 
@@ -12,3 +13,16 @@ afterAll(async () => {
 });
 
 export const testServer = supertest(server);
+
+// Helper function to create a test city
+export const createTestCity = async (
+  name: string = "Test City"
+): Promise<string> => {
+  const response = await testServer.post("/cities").send({ name });
+
+  if (response.statusCode !== StatusCodes.CREATED) {
+    throw new Error(`Failed to create test city: ${response.body}`);
+  }
+
+  return response.body;
+};
